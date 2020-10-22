75872363d2b11083c6f5b4dc8c87abc9546ba9ed
Sanvinganza <anton.shvetsov@mail.ru>
2020-10-22 04:00:39 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const primeNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const primeDecimalNumbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'ninteen'];
+    const decimalNumbers = [, , 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = 'hundred';
+
+    if (number.toString().length === 1) return primeNumbers[number];
+
+    if (number.toString().length === 2) {
+        if (+number.toString().split('')[0] === 1) return primeDecimalNumbers[number - 10];
+        return decimalNumbers[+number.toString().split('')[0]] + ' ' + (+number.toString().split('')[1] !== 0 ?
+            primeNumbers[+number.toString().split('')[1]] : '')
+    }
+
+    if (number.toString().length === 3) {
+        return primeNumbers[+number.toString().split('')[0]] + ' ' + hundred + ' ' +
+            decimalNumbers[+number.toString().split('')[1]] + ' ' + (+number.toString().split('')[2] !== 0 ?
+                primeNumbers[+number.toString().split('')[2]] : '')
+    }
+
+    return +number.toString().split('')[0]
 }
